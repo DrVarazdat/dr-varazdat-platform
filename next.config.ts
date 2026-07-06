e5478e97caaf3c1f54496f336b9264a5131dfda8
DrVarazdat <varazdat.platform@hotmail.com>
2026-07-06 19:33:55 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "gacwmibasnixdblvsxsm.supabase.co" } // <--- ALLOWS YOUR SUPABASE IMAGES!
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" }
     ],
+    unoptimized: true, // This completely disables Vercel's strict image blocking!
   },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true }
 };
+
 export default nextConfig;
