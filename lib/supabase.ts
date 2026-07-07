e6e3d3b8434a36c7b024e9325acf49acc1b17428
DrVarazdat <varazdat.platform@hotmail.com>
2026-07-07 14:22:51 +0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://gacwmibasnixdblvsxsm.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_9JsSXBlb7fWGEguCyyEiow_X_XRwbsv";
+// This will now perfectly use the keys you saved in Vercel and .env.local!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
